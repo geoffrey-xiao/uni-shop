34fd784dced653d4d8c9ddcdeea5e7c993e15c9d
xiaojianfeng <1377499035@qq.com>
2022-10-12 18:01:30 +0800

    完成商品列表开发

---
+++ components/goods-item/goods-item.vue
@@ -0,0 +1,48 @@
+<template>
+	<view>
+		<view class="goods-item" @click="goDetail(item)">
+			<image class="goods-img" :src="item.goods_small_logo"></image>
+			<view class="goods-detail">
+				<view class="goods-name">
+					{{item.goods_name | ''}}
+				</view>
+				<view class="goods-price">
+					${{item.goods_price | toFixed}}
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "goods-item",
+		props: {
+			item: {
+				type: Object,
+				default: null
+			}
+		},
+		filters: {
+			toFixed(num) {
+				return Number(num).toFixed(2)
+			}
+		},
+		data() {
+			return {
+
+			};
+		},
+		methods: {
+			goDetail() {
+				uni.navigateTo({
+					url: `/subpkg/goods-detail/goods-detail?goods_id=${this.item.goods_id}`,
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
--- subpkg/goods-list/goods-list.vue
+++ subpkg/goods-list/goods-list.vue
@@ -1,6 +1,8 @@
 <template>
 	<view>
-		
+		<view class="goods-list">
+			<goods-item v-for="item in goodsList" :key="item.goods_id" :item="item"></goods-item>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +10,78 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj: {
+					query: '',
+					cid: '5',
+					pageNum: 1,
+					pageSize: 10
+				},
+				goodsList: [],
+				total: 0,
+				loading: false
 			};
+		},
+		onLoad(options) {
+			this.queryObj.query = '';
+			this.queryObj.cid = options.cid || 5;
+			this.getGoodsList()
+		},
+		onReachBottom() {
+			if (this.queryObj.pageNum * this.queryObj.pageSize >= this.total) return uni.$showMsg('数据加载完毕')
+			if (loading) return
+			this.queryObj.pageNum += 1;
+			this.getGoodsList()
+		},
+
+		onPullDownRefresh() {
+			this.queryObj.pageNum = 1;
+			this.goodsList = [];
+			this.total = 0;
+			this.loading=false
+			this.getGoodsList(() => uni.stopPullDownRefresh())
+		},
+		methods: {
+			async getGoodsList(cb) {
+				this.loading = true
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				this.loading = false
+				cb && cb()
+				if (res.meta.status !== 200) return;
+				this.goodsList = [...this.goodsList, ...res.message.goods];
+				this.total = res.message.total
+				console.log(this.goodsList)
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	.goods-list {
+		.goods-item {
+			display: flex;
+			padding: 10px;
 
+			.goods-img {
+				display: block;
+				height: 100px;
+				margin-right: 5px;
+			}
+
+			.goods-detail {
+				font-size: 12px;
+				display: flex;
+				flex-direction: column;
+				justify-content: space-between;
+
+				.goods-name {}
+
+				.goods-price {
+					color: #c00000;
+				}
+
+			}
+		}
+	}
 </style>
